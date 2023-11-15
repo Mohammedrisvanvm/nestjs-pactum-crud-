2f2b2a0c772aecb21d93ea4133b1d72479e68d9f
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-15 18:49:38 +0530

    :wrench: bug fie

--- prisma/migrations/20231115131029_init/migration.sql
+++ prisma/migrations/20231115131029_init/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "User" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "email" TEXT NOT NULL,
-    "hash" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT,
 
