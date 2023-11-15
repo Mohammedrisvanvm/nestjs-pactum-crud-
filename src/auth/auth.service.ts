9389b8a12d71b7fe0fe510088525bc1de5284e20
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-15 12:48:14 +0530

    prisma module declared globally

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { User,BookMark } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable({})
 export class AuthService{
+    constructor(private prisma:PrismaService){}
 
     signin() {
         return {msg:'iam signin service'};
