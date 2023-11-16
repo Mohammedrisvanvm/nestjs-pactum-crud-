654a81fbc106b7f349c1f4f159dbb338db3dba42
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-16 12:31:25 +0530

    :wrench:config module from nest added

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import {PrismaClient} from '@prisma/client'
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client'
+
 
 @Injectable()
 export class PrismaService extends PrismaClient {
 
-    constructor(){
-        super({datasources:{
-            db:{url:'postgresql://postgres:123@localhost:5432/nest?schema=public'}
-        }})
+    constructor(config: ConfigService) {
+        super({
+            datasources: {
+                db: { url: config.get('DATABASE_URL') }
+            }
+        })
+        console.log(config.get('DATABASE_URL'));
+        
     }
 }
