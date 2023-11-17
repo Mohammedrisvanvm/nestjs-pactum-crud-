8913dc969c4ef73c54808a923442cc51dc338c7b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-17 11:16:48 +0530

    :sparkles: guards implemented separately

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,11 +3,12 @@ import { AuthGuard } from '@nestjs/passport'
 import { User } from '@prisma/client';
 import { Request } from 'express';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { jwtGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('users')
 export class UserController {
     // GET /users/me
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(jwtGuard)
     @Get('me')
     getMe(@GetUser() user: User) {
 
