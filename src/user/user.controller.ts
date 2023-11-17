d5a71447696949df69925cf4fca1941025613698
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-11-17 10:58:48 +0530

    :sparkles:created custom decorator for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get,UseGuards,Req } from '@nestjs/common';
+import { Controller, Get,UseGuards,Req, Patch } from '@nestjs/common';
 import {AuthGuard} from '@nestjs/passport'
+import { User } from '@prisma/client';
 import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+
 @Controller('users')
 export class UserController {
     // GET /users/me
     @UseGuards(AuthGuard('jwt'))
     @Get('me')
-    getMe(@Req() req:Request) {
+    getMe(@GetUser() user:User) {
         
-        return req.user
+        return user
     }
+     // PATCH /users/edit
+
+     @Patch()
+     editUser(){
+        
+     }
 }
